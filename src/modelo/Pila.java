95cfcf56fe702f41d4b87b21e055e6d53b702cc1
SebastianEsatupinan <124362855+SebastianEsatupinan@users.noreply.github.com>
2023-08-08 08:21:41 -0500

    Actualización 1
    Se agregaron los métodos.

--- src/modelo/Pila.java
+++ src/modelo/Pila.java
@@ -1,5 +1,42 @@
 package modelo;
 
-public class Pila {
+import java.util.LinkedList;
+
+public class Pila <T extends Base>{
+    
+    private LinkedList<T> tope;
+    
+    public Pila(){
+        
+    }
+
+    public Pila(LinkedList<T> tope) {
+        tope = new LinkedList<>();
+    }
+    
+    public void apilar(T elemento){
+        tope.addFirst(elemento);
+    }
+    
+    public void desapilar(){
+        
+    }
+    
+    public Boolean apilar(){
+        return tope.isEmpty();
+    }
+
+    @Override
+    public String toString() {
+        
+        StringBuilder sb = new StringBuilder("{");
+        
+        for(T elemento : tope){
+            sb.append("").append(elemento.toString());
+        }
+        sb.append("}");
+        return sb.toString();
+    }
+    
     
 }
