6d5f442fa28bba6ff0135fe906d44aa1b111fdcc
SebastianEsatupinan <124362855+SebastianEsatupinan@users.noreply.github.com>
2023-08-24 12:59:56 -0500

    Modificación 1 De Operaciones
    Se Empezó La implementación de las Operaciones.

---
+++ src/datos/persona.java
@@ -0,0 +1,76 @@
+package datos;
+
+public class persona {
+    
+    private String nombreP;
+    private String direcP;
+    private int edadP;
+
+    public persona(String nombreP, String direcP, int edadP) {
+        this.nombreP = nombreP;
+        this.direcP = direcP;
+        this.edadP = edadP;
+    }
+    
+    /**
+     * Get the value of edadP
+     *
+     * @return the value of edadP
+     */
+    public int getEdadP() {
+        return edadP;
+    }
+
+    /**
+     * Set the value of edadP
+     *
+     * @param edadP new value of edadP
+     */
+    public void setEdadP(int edadP) {
+        this.edadP = edadP;
+    }
+
+
+    /**
+     * Get the value of direcP
+     *
+     * @return the value of direcP
+     */
+    public String getDirecP() {
+        return direcP;
+    }
+
+    /**
+     * Set the value of direcP
+     *
+     * @param direcP new value of direcP
+     */
+    public void setDirecP(String direcP) {
+        this.direcP = direcP;
+    }
+
+
+    /**
+     * Get the value of nombreP
+     *
+     * @return the value of nombreP
+     */
+    public String getNombreP() {
+        return nombreP;
+    }
+
+    /**
+     * Set the value of nombreP
+     *
+     * @param nombreP new value of nombreP
+     */
+    public void setNombreP(String nombreP) {
+        this.nombreP = nombreP;
+    }
+
+    @Override
+    public String toString() {
+        return nombreP +  direcP +  edadP;
+    }
+    
+}
---
+++ src/modelo/operecionPila.java
@@ -0,0 +1,12 @@
+package modelo;
+
+public class operecionPila {
+    
+    public static <T extends Base> Pila<T> duplicarPila (Pila<T> pilaOriginal){
+        
+        Pila<T> pilaAux = new Pila<>();
+        Pila<T> pilaDuplicada = new Pila<>();
+        //while (!pilaOriginal)
+        return null;
+    }
+}
